b3b3a4ca298c63b795fa6238e32ce60f0b4da6c1
jillur984 <jillur.cse.bd@gmail.com>
2022-08-11 16:43:12 +0600

    JS Practice

---
+++ hello.js
@@ -0,0 +1,10 @@
+
+
+let a=40;
+let b=30;
+if(a>b){
+    console.log('A is greater than B')
+}
+else{
+    console.log('B is greater than A')
+}
---
+++ test.js
@@ -0,0 +1,247 @@
+//console.log(Math.E);
+//console.log(Math.PI);
+//console.log(4.589);
+// var n=4.589;
+// console.log(Math.abs(n));
+// console.log(Math.floor(n));
+// console.log(Math.ceil(n));
+// console.log(Math.round(n));
+// console.log(Math.max(200,400));
+// console.log(Math.max(400,450));
+// console.log(Math.pow(2,3));
+// console.log(Math.pow(3,8));
+// console.log(Math.sqrt(64));
+// console.log(Math.sqrt(9));
+// console.log(Math.round(Math.random()* 50 +1));
+
+
+// ..................Date javascript........
+// var date=new Date();
+// console.log(date.getDate());
+// console.log(date.getFullYear());
+// console.log(date.toLocaleDateString());
+// console.log(date.toTimeString());
+// console.log(date.getMonth());
+// console.log(date.getSeconds());
+
+
+
+//Javascript If Condition..............
+// var a=50;
+// var b=100;
+// if (a>b){
+//     console.log('A is greater than B');
+    
+// }
+// if (a<b){
+//     console.log('B is greater than A')
+
+// }
+
+// Even or ODD in If else.....
+// let n=10;
+// if (n%2===0){
+//     console.log(n + ` is a Even Number`);
+
+// }
+// if(n%2===1){
+//     console.log(n + ` is a Odd Number`);
+
+// }
+
+// ...........Else condition.........
+// let a=10;
+// let b=20;
+// if(a>b){
+//     console.log(' A is greater than B');
+
+// }
+// else{
+//     console.log('B is greater than A');
+// }
+
+
+
+// let n=13;
+// if (n%2===0){
+//     console.log(n + ` is a Even Number`);
+
+// }
+// else{
+//     console.log(n + ` is a Odd Number`);
+
+// }
+
+// ......Else if..........
+// let a=200;
+// let b=200;
+// if(a>b){
+//     console.log('A is greater than B');
+// }
+// else if(a<b){
+//     console.log('B is greater than A');
+// }
+// else{
+//     console.log('They are Equal number');
+// }
+
+
+
+// ..........Switch statement..............
+// var date= new Date();
+// var today=date.getDay()
+// switch(today){
+//     case 0:
+//         console.log('Today is Sunday');
+//         break;
+//         case 1:
+//         console.log('Today is Monday');
+//         break;
+//         case 2:
+//         console.log('Today is Tuesday');
+//         break;
+//         case 3:
+//         console.log('Today is Wednesday');
+//         break;
+//         case 4:
+//         console.log('Today is Thrusday');
+//         break;
+//         case 5:
+//         console.log('Today is Friday');
+//         break;
+//         case 6:
+//         console.log('Today is Saturday');
+//         break;
+//         default:
+//             console.log('Not a Valid Number')
+// }
+
+
+
+
+
+//  For Loop ......
+// for(var i=1; i<=100;i++){
+//     if (i%2===0){
+//         console.log(i);
+
+//     }
+
+
+
+
+
+// var sum=0;
+
+// for(var i=1;i<=100;i++){
+
+//     sum+=i;
+//     console.log(sum);
+
+// }
+
+
+// While Loop...
+// var i=0;
+// i++;
+// while (i<10){
+//     console.log('Jillur Rahman')
+// }
+
+
+//Do while loop........
+
+// var isRunning=false;
+// while(isRunning){
+//     console.log('I am running');
+// }
+// do{
+//     console.log('I am running');
+// }
+// while(isRunning);
+
+
+// string lenghth.......
+//kothin niyom
+// var str='Bangladesh is our motherland';
+// var length=0;
+// while(true){
+//     if(str.charAt(length)===''){
+//         break;
+       
+//     }
+//     else{
+//         length++
+//       }
+      
+    
+
+// }
+// console.log(length);
+
+//........................
+//Sohoj Niyom
+// var str='Bangladesh is our motherland'
+// console.log(str.length);
+
+//.......Array Chapter.................//
+
+//Easy Solution//
+// var arr=[1,2,3,4,5];
+// console.log(arr[4]);
+// console.log(arr[0]);
+// arr[10]=6;
+// console.log(arr);
+// console.log(arr[7]);
+// console.log(arr.length);
+// arr[2]=22;
+// console.log(arr);
+
+// var arr2=Array(1,2,3);//Using Constructor method
+// console.log(arr2);
+
+//...........Array Traversing................//
+// var arr=[4,5,1,6,8,9,40];
+// for(var i=0;i<arr.length;i++){
+//     arr[i]=arr[i]+2;
+// }
+// console.log(arr);
+
+//.............
+
+var arr=[4,5,1,6,8,9,40,70];
+// var sum=0;
+// for(var i=0;i<arr.length;i++){
+//     sum=sum+arr[i];
+
+// }
+// console.log(sum);
+
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]%2===1){//odd number
+//         console.log(arr[i]);
+//     }
+
+// }
+//..............
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]%2===0){//Even number
+//         console.log(arr[i]);
+//     }
+
+// }
+
+//.........Object...............//
+// var obj={}
+// obj.x=10;
+// console.log(obj);
+
+
+
+// var point={
+//     x:10,
+//     y:30
+// };
+// console.log(point);
+
+
